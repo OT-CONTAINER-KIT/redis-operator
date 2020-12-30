fbc7a57212a68a7c32df12d28df0c09e0708c840
iamabhishek-dubey <abhishekbhardwaj510@gmail.com>
2020-12-30 16:46:47 +0530

    Initial code for v0.1.X
    Signed-off-by: iamabhishek-dubey <abhishekbhardwaj510@gmail.com>

--- pkg/apis/redis/v1alpha1/doc.go
+++
@@ -1,4 +0,0 @@
-// Package v1alpha1 contains API Schema definitions for the redis v1alpha1 API group
-// +k8s:deepcopy-gen=package,register
-// +groupName=redis.opstreelabs.in
-package v1alpha1
--- version/version.go
+++
@@ -1,6 +0,0 @@
-package version
-
-var (
-	// Version is the defined version of Redis Operator
-	Version = "0.2"
-)
