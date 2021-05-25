3037a1a1b284843735a3e5d45d15a60cb4b9e2cc
lmulder <loic.mulder@its4u.lu>
2021-05-25 15:06:21 +0200

    Formatted k8sutils/services.go
    Signed-off-by: lmulder <loic.mulder@its4u.lu>

--- k8sutils/services.go
+++ k8sutils/services.go
@@ -110,8 +110,8 @@ func CreateMasterHeadlessService(cr *redisv1beta1.Redis) {
 // CreateMasterService creates different services for master
 func CreateMasterService(cr *redisv1beta1.Redis) {
 	labels := map[string]string{
-		"app":   cr.ObjectMeta.Name + "-master",
-		"role":  "master",
+		"app":  cr.ObjectMeta.Name + "-master",
+		"role": "master",
 	}
 	serviceDefinition := GenerateServiceDef(cr, labels, int32(redisPort), "master", cr.ObjectMeta.Name+"-master", cr.Spec.Master.Service.Type)
 	serviceBody, err := GenerateK8sClient().CoreV1().Services(cr.Namespace).Get(context.TODO(), cr.ObjectMeta.Name+"-master", metav1.GetOptions{})
@@ -142,8 +142,8 @@ func CreateSlaveHeadlessService(cr *redisv1beta1.Redis) {
 // CreateSlaveService creates different services for slave
 func CreateSlaveService(cr *redisv1beta1.Redis) {
 	labels := map[string]string{
-		"app":   cr.ObjectMeta.Name + "-slave",
-		"role":  "slave",
+		"app":  cr.ObjectMeta.Name + "-slave",
+		"role": "slave",
 	}
 	serviceDefinition := GenerateServiceDef(cr, labels, int32(redisPort), "slave", cr.ObjectMeta.Name+"-slave", cr.Spec.Slave.Service.Type)
 	serviceBody, err := GenerateK8sClient().CoreV1().Services(cr.Namespace).Get(context.TODO(), cr.ObjectMeta.Name+"-slave", metav1.GetOptions{})
@@ -158,8 +158,8 @@ func CreateSlaveService(cr *redisv1beta1.Redis) {
 // CreateStandaloneService creates redis standalone service
 func CreateStandaloneService(cr *redisv1beta1.Redis) {
 	labels := map[string]string{
-		"app":   cr.ObjectMeta.Name + "-" + "standalone",
-		"role":  "standalone",
+		"app":  cr.ObjectMeta.Name + "-" + "standalone",
+		"role": "standalone",
 	}
 	serviceDefinition := GenerateServiceDef(cr, labels, int32(redisPort), "standalone", cr.ObjectMeta.Name, cr.Spec.Service.Type)
 	serviceBody, err := GenerateK8sClient().CoreV1().Services(cr.Namespace).Get(context.TODO(), cr.ObjectMeta.Name, metav1.GetOptions{})
