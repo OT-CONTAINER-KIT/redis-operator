a526be4b964b75ca4f2fe642d2ff1af35cd0fe27
Abhishek Dubey <abhishekbhardwaj510@gmail.com>
2021-11-30 16:45:28 +0530

    [Development][Add] Fixed service for leader and service (#179)
    Signed-off-by: iamabhishek-dubey <abhishekbhardwaj510@gmail.com>

--- controllers/rediscluster_controller.go
+++ controllers/rediscluster_controller.go
@@ -61,17 +61,21 @@ func (r *RedisClusterReconciler) Reconcile(ctx context.Context, req ctrl.Request
 	if err != nil {
 		return ctrl.Result{}, err
 	}
-	err = k8sutils.CreateRedisLeaderService(instance)
-	if err != nil {
-		return ctrl.Result{}, err
+	if instance.Spec.RedisLeader.Replicas != nil && *instance.Spec.RedisLeader.Replicas != 0 {
+		err = k8sutils.CreateRedisLeaderService(instance)
+		if err != nil {
+			return ctrl.Result{}, err
+		}
 	}
 	err = k8sutils.CreateRedisFollower(instance)
 	if err != nil {
 		return ctrl.Result{}, err
 	}
-	err = k8sutils.CreateRedisFollowerService(instance)
-	if err != nil {
-		return ctrl.Result{}, err
+	if instance.Spec.RedisFollower.Replicas != nil && *instance.Spec.RedisFollower.Replicas != 0 {
+		err = k8sutils.CreateRedisFollowerService(instance)
+		if err != nil {
+			return ctrl.Result{}, err
+		}
 	}
 
 	redisLeaderInfo, err := k8sutils.GetStateFulSet(instance.Namespace, instance.ObjectMeta.Name+"-leader")
