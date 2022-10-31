6bbfd9218437ead294c10c977d18b96c6d8cfa53
iamabhishek-dubey <abhishekbhardwaj510@gmail.com>
2022-10-31 22:46:53 +0530

    Added logo without name
    Signed-off-by: iamabhishek-dubey <abhishekbhardwaj510@gmail.com>

--- docs/src/pages/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import useBaseUrl from "@docusaurus/useBaseUrl";
-
-import styles from './index.module.css';
-
-export default function Home() {
-  return <Redirect to={useBaseUrl("/getting-started/introduction")} />;
-}
