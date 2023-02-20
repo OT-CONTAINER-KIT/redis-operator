31dc005d220a993a71ff0416c1c3650d2b0ec8f2
Shubham Gupta <iamshubhamgupta2001@gmail.com>
2023-02-20 20:39:51 +0530

    Revert "Fixed StatefulSet(sentinel) Label for Service(Selector) #434 #435"
    This reverts commit 61f08296c26264b1acd848718afd64f2aa09d582.
    
    Signed-off-by: guozhi.li <guozhi.li@daocloud.io>

--- k8sutils/redis-sentinel.go
+++ k8sutils/redis-sentinel.go
@@ -57,7 +57,7 @@ func CreateRedisSentinelService(cr *redisv1beta1.RedisSentinel) error {
 // Create Redis Sentinel Cluster Setup
 func (service RedisSentinelSTS) CreateRedisSentinelSetup(cr *redisv1beta1.RedisSentinel) error {
 
-	stateFulName := cr.ObjectMeta.Name + "-" + service.RedisStateFulType
+	stateFulName := cr.ObjectMeta.Name
 	logger := statefulSetLogger(cr.Namespace, stateFulName)
 	labels := getRedisLabels(stateFulName, "cluster", service.RedisStateFulType, cr.ObjectMeta.Labels)
 	annotations := generateStatefulSetsAnots(cr.ObjectMeta)
