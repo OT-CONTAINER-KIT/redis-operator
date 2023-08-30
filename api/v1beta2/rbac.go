bd5d2cf27659780780b986ff2e64ad99a927b097
yangw <wuyangmuc@gmail.com>
2023-08-30 23:03:12 +0800

    [Fix]- rbac resource name (#598)
    * Fix rbac resource name
    
    Signed-off-by: drivebyer <wuyangmuc@gmail.com>
    
    * Execute make manifest
    
    Signed-off-by: drivebyer <wuyangmuc@gmail.com>
    
    ---------
    
    Signed-off-by: drivebyer <wuyangmuc@gmail.com>

--- api/v1beta2/rbac.go
+++ api/v1beta2/rbac.go
@@ -5,7 +5,7 @@ package v1beta2
 // +kubebuilder:rbac:groups=apiextensions.k8s.io,resources=customresourcedefinitions,verbs=get;list;watch
 // +kubebuilder:rbac:groups=redis.redis.opstreelabs.in,resources=redis/finalizers;rediscluster/finalizers;redissentinel/finalizers;redisreplication/finalizers,verbs=update
 // +kubebuilder:rbac:groups=redis.redis.opstreelabs.in,resources=redis/status;rediscluster/status;redissentinel/status;redisreplication/status,verbs=get;patch;update
-// +kubebuilder:rbac:groups=,resources=secrets;pods/exec;pods;services;configmaps;events;persistentvolumeclaims;namespace,verbs=create;delete;get;list;patch;update;watch
+// +kubebuilder:rbac:groups=,resources=secrets;pods/exec;pods;services;configmaps;events;persistentvolumeclaims;namespaces,verbs=create;delete;get;list;patch;update;watch
 // +kubebuilder:rbac:groups=apps,resources=statefulsets,verbs=create;delete;get;list;patch;update;watch
 // +kubebuilder:rbac:groups=coordination.k8s.io,resources=leases,verbs=create;delete;get;list;patch;update;watch
 // +kubebuilder:rbac:groups=policy,resources=poddisruptionbudgets,verbs=create;delete;get;list;patch;update;watch
