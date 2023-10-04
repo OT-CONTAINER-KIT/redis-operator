1c9999ae41ebaf0c3bbdfe88edb1f35ed4119505
titanventura <aswath7862001@gmail.com>
2023-10-04 09:33:33 +0530

    Refactor. run go fmt
    Signed-off-by: titanventura <aswath7862001@gmail.com>

--- api/v1beta2/redissentinel_webhook.go
+++ api/v1beta2/redissentinel_webhook.go
@@ -20,7 +20,6 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 )
 
-
 func (r *RedisSentinel) SetupWebhookWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewWebhookManagedBy(mgr).
 		For(r).
