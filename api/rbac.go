aecb51c3cb610bc707c5b277ddd057f1dfcc63dd
yangw <yang.wu@daocloud.io>
2023-10-07 14:20:30 +0800

    [Fix] - make deploy target not works well (#662)
    * execute make manifests
    
    Signed-off-by: drivebyer <yang.wu@daocloud.io>
    
    * fix indent
    
    Signed-off-by: drivebyer <yang.wu@daocloud.io>
    
    * match operator's name && namespace
    
    Signed-off-by: drivebyer <yang.wu@daocloud.io>
    
    * disable webhook related resources
    
    Signed-off-by: drivebyer <yang.wu@daocloud.io>
    
    * fix no-group name for core api resources
    
    Signed-off-by: drivebyer <yang.wu@daocloud.io>
    
    * disable manager auth by default
    
    Signed-off-by: drivebyer <yang.wu@daocloud.io>
    
    * fix ClusterRoleBinding
    
    Signed-off-by: drivebyer <yang.wu@daocloud.io>
    
    ---------
    
    Signed-off-by: drivebyer <yang.wu@daocloud.io>

--- api/rbac.go
+++ api/rbac.go
@@ -5,7 +5,7 @@ package api
 // +kubebuilder:rbac:groups=apiextensions.k8s.io,resources=customresourcedefinitions,verbs=get;list;watch
 // +kubebuilder:rbac:groups=redis.redis.opstreelabs.in,resources=redis/finalizers;rediscluster/finalizers;redisclusters/finalizers;redissentinel/finalizers;redissentinels/finalizers;redisreplication/finalizers;redisreplications/finalizers,verbs=update
 // +kubebuilder:rbac:groups=redis.redis.opstreelabs.in,resources=redis/status;rediscluster/status;redisclusters/status;redissentinel/status;redissentinels/status;redisreplication/status;redisreplications/status,verbs=get;patch;update
-// +kubebuilder:rbac:groups=,resources=secrets;pods/exec;pods;services;configmaps;events;persistentvolumeclaims;namespaces,verbs=create;delete;get;list;patch;update;watch
+// +kubebuilder:rbac:groups="",resources=secrets;pods/exec;pods;services;configmaps;events;persistentvolumeclaims;namespaces,verbs=create;delete;get;list;patch;update;watch
 // +kubebuilder:rbac:groups=apps,resources=statefulsets,verbs=create;delete;get;list;patch;update;watch
 // +kubebuilder:rbac:groups=coordination.k8s.io,resources=leases,verbs=create;delete;get;list;patch;update;watch
 // +kubebuilder:rbac:groups=policy,resources=poddisruptionbudgets,verbs=create;delete;get;list;patch;update;watch
