b42477592ec1918b5f6269772c7ede9084b0f2ce
Shubham Gupta <iamshubhamgupta2001@gmail.com>
2023-11-09 02:42:15 +0530

    client_test.go
    Signed-off-by: Shubham Gupta <iamshubhamgupta2001@gmail.com>

---
+++ k8sutils/client_test.go
@@ -0,0 +1,78 @@
+package k8sutils
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"k8s.io/client-go/rest"
+)
+
+func mockK8sConfigProvider() (*rest.Config, error) {
+	return &rest.Config{}, nil
+}
+func mockInvalidK8sConfigProvider() (*rest.Config, error) {
+	return nil, errors.New("invalid configuration")
+}
+
+func TestGenerateK8sClient(t *testing.T) {
+	tests := []struct {
+		name           string
+		configProvider func() (*rest.Config, error)
+		wantErr        bool
+	}{
+		{
+			name:           "valid config",
+			configProvider: mockK8sConfigProvider,
+			wantErr:        false,
+		},
+		{
+			name:           "invalid config",
+			configProvider: mockInvalidK8sConfigProvider,
+			wantErr:        true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			client, err := generateK8sClient(tt.configProvider)
+			if tt.wantErr {
+				assert.Error(t, err, "generateK8sClient() should return an error for invalid config")
+			} else {
+				assert.NoError(t, err, "generateK8sClient() should not return an error for valid config")
+				assert.NotNil(t, client, "expected a non-nil Kubernetes client")
+			}
+		})
+	}
+}
+
+func TestGenerateK8sDynamicClient(t *testing.T) {
+	tests := []struct {
+		name           string
+		configProvider func() (*rest.Config, error)
+		wantErr        bool
+	}{
+		{
+			name:           "valid config",
+			configProvider: mockK8sConfigProvider,
+			wantErr:        false,
+		},
+		{
+			name:           "invalid config",
+			configProvider: mockInvalidK8sConfigProvider,
+			wantErr:        true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			client, err := generateK8sDynamicClient(tt.configProvider)
+			if tt.wantErr {
+				assert.Error(t, err, "generateK8sDynamicClient() should return an error for invalid config")
+			} else {
+				assert.NoError(t, err, "generateK8sDynamicClient() should not return an error for valid config")
+				assert.NotNil(t, client, "expected a non-nil Kubernetes client")
+			}
+		})
+	}
+}
