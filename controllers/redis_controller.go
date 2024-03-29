bf1a5a90dfbb645ed9c9c6ebd57b4dddaeb94813
Mathieu Cesbron <45229023+MathieuCesbron@users.noreply.github.com>
2024-03-29 13:32:31 +0100

    Remove useless comments (#761)
    Signed-off-by: Mathieu Cesbron <mathieu.cesbron@protonmail.com>

--- controllers/redis_controller.go
+++ controllers/redis_controller.go
@@ -40,7 +40,6 @@ type RedisReconciler struct {
 	Scheme     *runtime.Scheme
 }
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims
 func (r *RedisReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	reqLogger := r.Log.WithValues("Request.Namespace", req.Namespace, "Request.Name", req.Name)
 	reqLogger.Info("Reconciling opstree redis controller")
