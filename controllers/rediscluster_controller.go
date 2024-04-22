736836c39ed9d2e0361fe5d72f6f8eff81660972
yangw <yang.wu@daocloud.io>
2024-04-22 16:41:59 +0800

    fix: should get the really leader count when scale in (#885)
    * fix: should get the really leader count when scale in
    
    Signed-off-by: drivebyer <yang.wu@daocloud.io>
    
    * fix timeout
    
    Signed-off-by: drivebyer <yang.wu@daocloud.io>
    
    * fix test
    
    Signed-off-by: drivebyer <yang.wu@daocloud.io>
    
    ---------
    
    Signed-off-by: drivebyer <yang.wu@daocloud.io>

--- controllers/rediscluster_controller.go
+++ controllers/rediscluster_controller.go
@@ -74,14 +74,10 @@ func (r *RedisClusterReconciler) Reconcile(ctx context.Context, req ctrl.Request
 	}
 
 	// Check if the cluster is downscaled
-	if leaderReplicas < instance.Status.ReadyLeaderReplicas {
-		reqLogger.Info("Redis cluster is downscaling...", "Ready.ReadyLeaderReplicas", instance.Status.ReadyLeaderReplicas, "Expected.ReadyLeaderReplicas", leaderReplicas)
-
-		// loop count times to remove the latest leader/follower pod
-		count := instance.Status.ReadyLeaderReplicas - leaderReplicas
-		for i := int32(0); i < count; i++ {
-			reqLogger.Info("Redis cluster is downscaling", "The times of loop", i)
-
+	if leaderCount := k8sutils.CheckRedisNodeCount(ctx, r.K8sClient, r.Log, instance, "leader"); leaderReplicas < leaderCount {
+		reqLogger.Info("Redis cluster is downscaling...", "Current.LeaderReplicas", leaderCount, "Desired.LeaderReplicas", leaderReplicas)
+		for shardIdx := leaderCount - 1; shardIdx >= leaderReplicas; shardIdx-- {
+			reqLogger.Info("Remove the shard", "Shard.Index", shardIdx)
 			//  Imp if the last index of leader sts is not leader make it then
 			// check whether the redis is leader or not ?
 			// if not true then make it leader pod
@@ -100,11 +96,7 @@ func (r *RedisClusterReconciler) Reconcile(ctx context.Context, req ctrl.Request
 		// Step 3 Rebalance the cluster
 		k8sutils.RebalanceRedisCluster(r.K8sClient, r.Log, instance)
 		reqLogger.Info("Redis cluster is downscaled... Rebalancing the cluster is done")
-		err = k8sutils.UpdateRedisClusterStatus(instance, status.RedisClusterReady, status.ReadyClusterReason, leaderReplicas, leaderReplicas, r.Dk8sClient)
-		if err != nil {
-			return ctrl.Result{}, err
-		}
-		return ctrl.Result{RequeueAfter: time.Second * 60}, nil
+		return ctrl.Result{RequeueAfter: time.Second * 10}, nil
 	}
 
 	// Mark the cluster status as initializing if there are no leader or follower nodes
