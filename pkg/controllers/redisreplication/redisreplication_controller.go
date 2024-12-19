2c7022a578916c8597725c89064b28358841ff72
yangw <yang.wu@daocloud.io>
2024-12-19 23:23:59 +0800

    feat(sentinel): enhance RedisSentinel reconciliation logic and update workflow. (#1176)
    feat: enhance RedisSentinel reconciliation logic and update workflow.
    
    - Added new reconciler functions for handling finalizers, annotations, replication, and sentinel management in the RedisSentinel controller.
    - Improved error handling and logging for better debugging during reconciliation.
    - Updated GitHub Actions workflow to include new job types: standalone, replication, sentinel, and cluster.
    
    This update enhances the functionality and maintainability of the RedisSentinel operator.
    
    Signed-off-by: yangw <wuyangmuc@gmail.com>

--- pkg/controllers/redisreplication/redisreplication_controller.go
+++ pkg/controllers/redisreplication/redisreplication_controller.go
@@ -127,6 +127,7 @@ func (r *Reconciler) reconcileFinalizer(ctx context.Context, instance *redisv1be
 
 func (r *Reconciler) reconcileAnnotation(ctx context.Context, instance *redisv1beta2.RedisReplication) (ctrl.Result, error) {
 	if _, found := instance.ObjectMeta.GetAnnotations()["redisreplication.opstreelabs.in/skip-reconcile"]; found {
+		log.FromContext(ctx).Info("found skip reconcile annotation", "namespace", instance.Namespace, "name", instance.Name)
 		return intctrlutil.RequeueAfter(ctx, time.Second*10, "found skip reconcile annotation")
 	}
 	return intctrlutil.Reconciled()
