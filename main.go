4e7b70fe41133c4b9b5edf9cc46c76baaefd9591
Husni Alhamdani <dhanielluis@gmail.com>
2025-02-18 13:14:21 +0700

    feat: configurable operator maxConcurrentReconciles
    Signed-off-by: Husni Alhamdani <dhanielluis@gmail.com>

--- main.go
+++ main.go
@@ -19,7 +19,6 @@ package main
 import (
 	"flag"
 	"os"
-	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"strconv"
 	"strings"
 
@@ -38,6 +37,7 @@ import (
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/cache"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	"sigs.k8s.io/controller-runtime/pkg/metrics/server"
