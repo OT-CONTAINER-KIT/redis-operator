0ac9ba3d563caa999c68897fcbcbaa69e96d69a9
drivebyer <wuyangmuc@gmail.com>
2025-02-19 17:52:10 +0800

    fix
    Signed-off-by: drivebyer <wuyangmuc@gmail.com>

--- main.go
+++ main.go
@@ -54,6 +54,7 @@ var (
 	setupLog = ctrl.Log.WithName("setup")
 )
 
+//nolint:gochecknoinits
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 	utilruntime.Must(redisv1beta2.AddToScheme(scheme))
