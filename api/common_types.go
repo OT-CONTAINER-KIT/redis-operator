3caf1fce4bec19e2555a1d55f3a8a4421d3450d0
yangw <wuyangmuc@gmail.com>
2025-02-28 10:38:54 +0800

    fix
    Signed-off-by: yangw <wuyangmuc@gmail.com>

--- api/common_types.go
+++ api/common_types.go
@@ -193,9 +193,8 @@ type RedisSentinelConfig struct {
 	FailoverTimeout string `json:"failoverTimeout,omitempty"`
 	// +kubebuilder:default:="30000"
 	DownAfterMilliseconds string `json:"downAfterMilliseconds,omitempty"`
-	// +kubebuilder:default:="30000"
+	// +kubebuilder:default:="no"
 	ResolveHostnames string `json:"resolveHostnames,omitempty"`
 	// +kubebuilder:default:="no"
 	AnnounceHostnames string `json:"announceHostnames,omitempty"`
-	// +kubebuilder:default:="no"
 }
