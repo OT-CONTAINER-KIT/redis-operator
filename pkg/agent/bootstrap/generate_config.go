5cc3da9b8bea1b00739ff3005772aabaf2d1703b
yangw <yang.wu@daocloud.io>
2025-03-04 16:57:15 +0800

    feat: add redis agent with bootstrap configuration generation (#1254)
    * move main
    
    Signed-off-by: drivebyer <wuyangmuc@gmail.com>
    
    * feat: add redis agent with bootstrap configuration generation
    
    - Introduced new agent package for Redis and Sentinel configuration management
    - Added Cobra CLI support for agent bootstrap command
    - Implemented configuration generation for Sentinel with environment-based customization
    - Created Dockerfile for agent image
    - Added utility functions for environment variable handling
    - Prepared foundation for Redis and Sentinel configuration bootstrapping
    
    Signed-off-by: Redis Operator Team
    Signed-off-by: drivebyer <wuyangmuc@gmail.com>
    
    * fix
    
    Signed-off-by: drivebyer <wuyangmuc@gmail.com>
    
    * fix
    
    Signed-off-by: drivebyer <wuyangmuc@gmail.com>
    
    ---------
    
    Signed-off-by: drivebyer <wuyangmuc@gmail.com>
    Signed-off-by: Redis Operator Team
    Signed-off-by: Muhammad Qadora <memomq70@gmail.com>

---
+++ pkg/agent/bootstrap/generate_config.go
@@ -0,0 +1,70 @@
+package bootstrap
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/spf13/cobra"
+)
+
+// agent bootstrap --sentinel: use to init config for sentinel or redis, if --sentinel is not set, it will init config for redis
+var (
+	BootstrapCmd = &cobra.Command{
+		Use:   "bootstrap",
+		Short: "Bootstrap do some init work before run redis/sentinel",
+		Run:   bootstrapRun,
+	}
+)
+
+//nolint:gochecknoinits
+func init() {
+	BootstrapCmd.Flags().Bool("sentinel", false, "Generate sentinel config instead of redis config")
+}
+
+func bootstrapRun(cmd *cobra.Command, args []string) {
+	generateConfig(cmd, args)
+}
+
+func generateConfig(cmd *cobra.Command, args []string) {
+	sentinel, err := cmd.Flags().GetBool("sentinel")
+	if err != nil {
+		log.Fatalf("Failed to get sentinel flag: %v", err)
+	}
+
+	if sentinel {
+		if err := generateSentinelConfig(); err != nil {
+			log.Fatalf("Failed to generate sentinel config: %v", err)
+		}
+	} else {
+		if err := generateRedisConfig(); err != nil {
+			log.Fatalf("Failed to generate redis config: %v", err)
+		}
+	}
+}
+
+type config struct {
+	content string
+	path    string
+}
+
+func newConfig(path string, defaultConfig ...string) *config {
+	return &config{
+		path:    path,
+		content: strings.Join(defaultConfig, "\n"),
+	}
+}
+
+// append config[0] as key, config[1]... as value
+func (c *config) append(config ...string) *config {
+	if len(config) == 0 {
+		return c
+	}
+	c.content = fmt.Sprintf("%s\n%s %s", c.content, config[0], strings.Join(config[1:], " "))
+	return c
+}
+
+func (c *config) commit() error {
+	return os.WriteFile(c.path, []byte(c.content), 0o644)
+}
--- pkg/util/coalesce.go
+++ pkg/util/coalesce.go
@@ -1,9 +1,19 @@
 package util
 
+import "os"
+
 func Coalesce[T comparable](val, defaultVal T) T {
 	var t T
 	if val == t {
 		return defaultVal
 	}
 	return val
+}
+
+func CoalesceEnv(key, defaultVal string) (string, bool) {
+	val := os.Getenv(key)
+	if val == "" {
+		return defaultVal, false
+	}
+	return val, true
 }
