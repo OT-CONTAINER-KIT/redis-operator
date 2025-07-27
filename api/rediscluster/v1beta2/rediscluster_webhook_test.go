4ebb5539c3f871f6e75281a734fff677570c75e0
yangw <wuyangmuc@gmail.com>
2025-07-27 16:47:03 +0800

    feat: Implement RedisCluster clusterSize validation webhook
    Signed-off-by: yangw <wuyangmuc@gmail.com>

---
+++ api/rediscluster/v1beta2/rediscluster_webhook_test.go
@@ -0,0 +1,68 @@
+package v1beta2_test
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+
+	v1beta2 "github.com/OT-CONTAINER-KIT/redis-operator/api/rediscluster/v1beta2"
+	"github.com/OT-CONTAINER-KIT/redis-operator/internal/testutil/webhook"
+	"github.com/stretchr/testify/require"
+	admissionv1beta1 "k8s.io/api/admission/v1beta1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/utils/ptr"
+)
+
+func TestRedisClusterWebhook(t *testing.T) {
+	cases := []webhook.ValidationWebhookTestCase{
+		{
+			Name:      "success-create-v1beta2-rediscluster-validate-clusterSize-3",
+			Operation: admissionv1beta1.Create,
+			Object: func(t *testing.T, uid string) []byte {
+				t.Helper()
+				cluster := mkRedisCluster(uid)
+				cluster.Spec.Size = ptr.To(int32(3))
+				return marshal(t, cluster)
+			},
+			Check: webhook.ValidationWebhookSucceeded,
+		},
+		{
+			Name:      "failed-create-v1beta2-rediscluster-validate-clusterSize-2",
+			Operation: admissionv1beta1.Create,
+			Object: func(t *testing.T, uid string) []byte {
+				t.Helper()
+				cluster := mkRedisCluster(uid)
+				cluster.Spec.Size = ptr.To(int32(2))
+				return marshal(t, cluster)
+			},
+			Check: webhook.ValidationWebhookFailed("Redis cluster must have at least 3 shards"),
+		},
+	}
+
+	gvk := metav1.GroupVersionKind{
+		Group:   "redis.redis.opstreelabs.in",
+		Version: "v1beta2",
+		Kind:    "RedisCluster",
+	}
+
+	cluster := &v1beta2.RedisCluster{}
+	webhook.RunValidationWebhookTests(t, gvk, cluster, cases...)
+}
+
+func mkRedisCluster(uid string) *v1beta2.RedisCluster {
+	return &v1beta2.RedisCluster{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: fmt.Sprintf("test-%s", uid),
+			UID:  types.UID(fmt.Sprintf("test-%s", uid)),
+		},
+		Spec: v1beta2.RedisClusterSpec{},
+	}
+}
+
+func marshal(t *testing.T, obj interface{}) []byte {
+	t.Helper()
+	bytes, err := json.Marshal(obj)
+	require.NoError(t, err)
+	return bytes
+}
