f5018612e78eaed54b49a46f5f6e7dc826043913
yangw <wuyangmuc@gmail.com>
2025-08-07 14:05:01 +0800

    fix lint
    Signed-off-by: yangw <wuyangmuc@gmail.com>

--- internal/controller/redisreplication/redisreplication_controller.go
+++ internal/controller/redisreplication/redisreplication_controller.go
@@ -4,19 +4,18 @@ import (
 	"context"
 	"time"
 
-	appsv1 "k8s.io/api/apps/v1"
-	"k8s.io/client-go/kubernetes"
-	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/controller"
-	"sigs.k8s.io/controller-runtime/pkg/log"
-
 	rrvb2 "github.com/OT-CONTAINER-KIT/redis-operator/api/redisreplication/v1beta2"
 	"github.com/OT-CONTAINER-KIT/redis-operator/internal/controller/common"
 	redis "github.com/OT-CONTAINER-KIT/redis-operator/internal/controller/common/redis"
 	intctrlutil "github.com/OT-CONTAINER-KIT/redis-operator/internal/controllerutil"
 	"github.com/OT-CONTAINER-KIT/redis-operator/internal/k8sutils"
 	"github.com/OT-CONTAINER-KIT/redis-operator/internal/monitoring"
+	appsv1 "k8s.io/api/apps/v1"
+	"k8s.io/client-go/kubernetes"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/log"
 )
 
 const (
@@ -160,11 +159,11 @@ func (r *Reconciler) reconcileRedis(ctx context.Context, instance *rrvb2.RedisRe
 	} else if len(masterNodes) == 1 && len(slaveNodes) > 0 {
 		realMaster = masterNodes[0]
 		currentRealMaster := k8sutils.GetRedisReplicationRealMaster(ctx, r.K8sClient, instance, masterNodes)
-		
+
 		if currentRealMaster == "" {
-			log.FromContext(ctx).Info("Detected disconnected slaves, reconfiguring replication", 
+			log.FromContext(ctx).Info("Detected disconnected slaves, reconfiguring replication",
 				"master", realMaster, "slaves", slaveNodes)
-			
+
 			allPods := append(masterNodes, slaveNodes...)
 			if err := k8sutils.CreateMasterSlaveReplication(ctx, r.K8sClient, instance, allPods, realMaster); err != nil {
 				log.FromContext(ctx).Error(err, "Failed to reconfigure master-slave replication",
