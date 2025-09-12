23d744b667f5a1871cf44d45d372dabee6b030dd
Mario Trangoni <mjtrangoni@gmail.com>
2025-09-12 10:49:45 +0200

    chore(golangci): fix noctx issue to use (*net.Dialer).DialContext
    See,
    $ golangci-lint run ./...
    internal/util/net.go:6:23: net.Dial must not be called. use (*net.Dialer).DialContext (noctx)
            conn, err := net.Dial("udp", "8.8.8.8:80")
                                 ^
    1 issues:
    * noctx: 1
    
    Signed-off-by: Mario Trangoni <mjtrangoni@gmail.com>

--- internal/util/net.go
+++ internal/util/net.go
@@ -1,9 +1,17 @@
 package util
 
-import "net"
+import (
+	"context"
+	"net"
+	"time"
+)
 
 func GetLocalIP() (string, error) {
-	conn, err := net.Dial("udp", "8.8.8.8:80")
+	dialer := net.Dialer{}
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+
+	conn, err := dialer.DialContext(ctx, "udp", "8.8.8.8:80")
 	if err != nil {
 		return "", err
 	}
